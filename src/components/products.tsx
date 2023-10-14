e19676458955d2639ea6de962372e03506fbcfb3
Soda Zero <soda.zero.sip@proton.me>
2023-10-14 01:30:28 -0300

    not found products

--- src/components/products.tsx
+++ src/components/products.tsx
@@ -1,6 +1,7 @@
 import { Posts } from "@/types/posts";
 import Image from "next/image";
 import Link from "next/link";
+import { BrokenHIcon } from "./ui/icons";
 
 export default function Products({
   posts,
@@ -18,6 +19,7 @@ export default function Products({
       )
     : newest5Posts;
   const showLast5Message = filteredPosts === newest5Posts;
+  const noProducts = filteredPosts.length === 0;
 
   return (
     <section className="w-full flex flex-col gap-4">
@@ -27,44 +29,52 @@ export default function Products({
         </header>
       )}
       <div className="flex gap-4 flex-col p-4">
-        {filteredPosts.map((post) => {
-          const date = new Date(Number(post.created_at) * 1000);
-          const day = date.getDate();
-          const month = date.getMonth() + 1;
-          const year = date.getFullYear();
-          const formattedDate = `${day}/${month}/${year}`;
-          if (!post.ignore) {
-            return (
-              <figure
-                className="rounded hover:scale-[1.01] transition-all group/product"
-                key={post.id}
-              >
-                <Link
-                  href={`https://www.instagram.com/p/${post.shortcode}/?img_index=1`}
-                  target="_blank"
-                  className="flex gap-2"
+        {noProducts ? (
+          <p className="flex items-center  justify-center flex-col">
+            No se encontró ningún producto...
+            <BrokenHIcon />
+          </p>
+        ) : (
+          filteredPosts.map((post) => {
+            const date = new Date(Number(post.created_at) * 1000);
+            const day = date.getDate();
+            const month = date.getMonth() + 1;
+            const year = date.getFullYear();
+            const formattedDate = `${day}/${month}/${year}`;
+
+            if (!post.ignore) {
+              return (
+                <figure
+                  className="rounded hover:scale-[1.01] transition-all group/product"
+                  key={post.id}
                 >
-                  <Image
-                    src={post.img_src}
-                    width={post.dimensions.width}
-                    height={post.dimensions.height}
-                    alt={`Imagen de: ${post.title}`}
-                    className="w-[150px] aspect-[3/4] h-auto shadow-[5px_5px_0px_0px_rgba(16,185,19,0.5)] rounded object-cover"
-                  />
-                  <figcaption className="bg-inherit p-2">
-                    <header className=" h-full w-full text-xl  group-hover/product:text-emerald-500">
-                      {post.title}
-                    </header>
-                    <footer className="text-xs text-zinc-700">
-                      {formattedDate}
-                    </footer>
-                  </figcaption>
-                </Link>
-                <div className="w-full border-b-2 pt-4 group-hover/product:border-emerald-500 border-black squiggle"></div>
-              </figure>
-            );
-          }
-        })}
+                  <Link
+                    href={`https://www.instagram.com/p/${post.shortcode}/?img_index=1`}
+                    target="_blank"
+                    className="flex gap-2"
+                  >
+                    <Image
+                      src={post.img_src}
+                      width={post.dimensions.width}
+                      height={post.dimensions.height}
+                      alt={`Imagen de: ${post.title}`}
+                      className="w-[150px] aspect-[3/4] h-auto shadow-[5px_5px_0px_0px_rgba(16,185,19,0.5)] rounded object-cover"
+                    />
+                    <figcaption className="bg-inherit p-2">
+                      <header className=" h-full w-full text-xl  group-hover/product:text-emerald-500">
+                        {post.title}
+                      </header>
+                      <footer className="text-xs text-zinc-700">
+                        {formattedDate}
+                      </footer>
+                    </figcaption>
+                  </Link>
+                  <div className="w-full border-b-2 pt-4 group-hover/product:border-emerald-500 border-black squiggle"></div>
+                </figure>
+              );
+            }
+          })
+        )}
       </div>
     </section>
   );
