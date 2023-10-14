94243ff1f8b40fa5ff938490b4add6b357703cb0
Soda Zero <soda.zero.sip@proton.me>
2023-10-14 03:27:25 -0300

    fixed link not reload

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Nav() {
   return (
     <nav className="sticky top-0 z-[999] bg-zinc-50 h-16 w-full">
       <div className="flex justify-between max-w-4xl w-full items-center h-full mx-auto">
-        <a href="/" className="px-5">
+        <Link href="/" className="px-5">
           <Image
             src={"/vxa.webp"}
             alt="conejo logo de veganoporaccidente.arg"
@@ -12,7 +13,7 @@ export default function Nav() {
             height={150}
             className="w-12 h-12"
           />
-        </a>
+        </Link>
       </div>
       <div className="w-full border-b-2 border-black squiggle"></div>
     </nav>
