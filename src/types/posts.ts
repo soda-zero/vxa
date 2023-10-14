93f32158d4d8dd953b11320e0de0b58d0c8a59fc
Soda Zero <soda.zero.sip@proton.me>
2023-10-14 01:43:33 -0300

    fixed types

--- src/types/posts.ts
+++ src/types/posts.ts
@@ -1,16 +1,16 @@
-
 export type Posts = Post[];
 export interface Post {
-id: string;
-title: string,
-tags: string;
-created_at: number;
-shortcode: string;
-dimensions: Dimensions;
-img_src: string;
+  id: string;
+  title: string;
+  tags: string;
+  created_at: number;
+  shortcode: string;
+  dimensions: Dimensions;
+  img_src: string;
+  ignore?: true;
 }
 
 export interface Dimensions {
-height: number;
-width: number;
+  height: number;
+  width: number;
 }
