6806e6070c2cabb3c0776e08dac2946ca3764169
soda zero <soda.zero.sip@proton.me>
2024-01-01 02:58:20 -0300

    added nix flake + rmeoved ts from the script

--- scripts/scrap_ig.mjs
+++ scripts/scrap_ig.mjs
@@ -2,203 +2,21 @@ import axios from "axios";
 import fs from "fs";
 import path from "path";
 
-export interface Root {
-  data: Data;
-  extensions: Extensions;
-  status: string;
-}
-
-export interface Data {
-  user: User;
-}
-
-export interface User {
-  edge_owner_to_timeline_media: EdgeOwnerToTimelineMedia;
-}
-
-export interface EdgeOwnerToTimelineMedia {
-  count: number;
-  page_info: PageInfo;
-  edges: Edge[];
-}
-
-export interface PageInfo {
-  has_next_page: boolean;
-  end_cursor: string;
-}
-
-export interface Edge {
-  node: Post;
-}
-
-export interface Post {
-  __typename: string;
-  id: string;
-  dimensions: Dimensions;
-  display_url: string;
-  display_resources: DisplayResource[];
-  is_video: boolean;
-  tracking_token: string;
-  edge_media_to_tagged_user: EdgeMediaToTaggedUser;
-  dash_info?: DashInfo;
-  video_url?: string;
-  video_view_count?: number;
-  edge_media_to_caption: EdgeMediaToCaption;
-  shortcode: string;
-  edge_media_to_comment: EdgeMediaToComment;
-  edge_media_to_sponsor_user: EdgeMediaToSponsorUser;
-  comments_disabled: boolean;
-  taken_at_timestamp: number;
-  edge_media_preview_like: EdgeMediaPreviewLike;
-  gating_info: any;
-  fact_check_overall_rating: any;
-  fact_check_information: any;
-  media_preview?: string;
-  owner: Owner2;
-  location: any;
-  viewer_has_liked: boolean;
-  viewer_has_saved: boolean;
-  viewer_has_saved_to_collection: boolean;
-  viewer_in_photo_of_you: boolean;
-  viewer_can_reshare: boolean;
-  thumbnail_src: string;
-  thumbnail_resources: ThumbnailResource[];
-  accessibility_caption: any;
-  edge_sidecar_to_children?: EdgeSidecarToChildren;
-}
-
-export interface Dimensions {
-  height: number;
-  width: number;
-}
-
-export interface DisplayResource {
-  src: string;
-  config_width: number;
-  config_height: number;
-}
-
-export interface EdgeMediaToTaggedUser {
-  edges: any[];
-}
-
-export interface DashInfo {
-  is_dash_eligible: boolean;
-  video_dash_manifest: string;
-  number_of_qualities: number;
-}
-
-export interface EdgeMediaToCaption {
-  edges: Edge2[];
-}
-
-export interface Edge2 {
-  node: Node2;
-}
-
-export interface Node2 {
-  text: string;
-}
-
-export interface EdgeMediaToComment {
-  count: number;
-  page_info: PageInfo2;
-  edges: Edge3[];
-}
-
-export interface PageInfo2 {
-  has_next_page: boolean;
-  end_cursor?: string;
-}
-
-export interface Edge3 {
-  node: Node3;
-}
-
-export interface Node3 {
-  id: string;
-  text: string;
-  created_at: number;
-  did_report_as_spam: boolean;
-  owner: Owner;
-  viewer_has_liked: boolean;
-}
-
-export interface Owner {
-  id: string;
-  is_verified: boolean;
-  profile_pic_url: string;
-  username: string;
-}
-
-export interface EdgeMediaToSponsorUser {
-  edges: any[];
-}
-
-export interface EdgeMediaPreviewLike {
-  count: number;
-  edges: Edge4[];
-}
-
-export interface Edge4 {
-  node: Node4;
-}
-
-export interface Node4 {
-  id: string;
-  profile_pic_url: string;
-  username: string;
-}
-
-export interface Owner2 {
-  id: string;
-  username: string;
-}
-
-export interface ThumbnailResource {
-  src: string;
-  config_width: number;
-  config_height: number;
-}
-
-export interface EdgeSidecarToChildren {
-  edges: Edge5[];
-}
-
-export interface Edge5 {
-  node: Node5;
-}
-
-export interface Node5 {
-  __typename: string;
-  id: string;
-  dimensions: Dimensions2;
-  display_url: string;
-  display_resources: DisplayResource2[];
-  is_video: boolean;
-  tracking_token: string;
-  edge_media_to_tagged_user: EdgeMediaToTaggedUser2;
-  accessibility_caption: any;
-}
-
-export interface Dimensions2 {
-  height: number;
-  width: number;
-}
-
-export interface DisplayResource2 {
-  src: string;
-  config_width: number;
-  config_height: number;
-}
-
-export interface EdgeMediaToTaggedUser2 {
-  edges: any[];
-}
-
-export interface Extensions {
-  is_final: boolean;
-}
+/**
+ * @typedef {Object} Dimensions - The dimensions of the Instagram post image.
+ * @property {number} height - The height of the image.
+ * @property {number} width - The width of the image.
+ *
+ * @typedef {Object} Post - Instagram post data.
+ * @property {string} id - The unique identifier of the post.
+ * @property {string} title - The title of the post.
+ * @property {string} tags - The tags associated with the post.
+ * @property {number} created_at - The timestamp when the post was created.
+ * @property {string} shortcode - The shortcode of the post.
+ * @property {Dimensions} dimensions - The dimensions of the post's image.
+ * @property {string} img_src - The source path of the post's image.
+ * @property {boolean} [ignore] - Optional field indicating whether the post should be ignored.
+ */
 
 const INSTAGRAM_APP_ID = "936619743392459";
 
@@ -224,7 +42,13 @@ const INSTAGRAM_APP_ID = "936619743392459";
 //   }
 // }
 
-async function scrape_post(urlOrShortcode: string) {
+/**
+ * Scrapes an Instagram post using its URL or shortcode.
+ *
+ * @param {string} urlOrShortcode - The URL or shortcode of the Instagram post.
+ * @returns {Promise<Object>} - A promise that resolves to the scraped data of the Instagram post.
+ */
+async function scrape_post(urlOrShortcode) {
   let shortcode = urlOrShortcode;
 
   if (urlOrShortcode.includes("http")) {
@@ -257,8 +81,15 @@ async function scrape_post(urlOrShortcode: string) {
   return data.data.shortcode_media;
 }
 
-// Last time was page 70
-async function scrape_user_posts(user_id: string, page_size = 12) {
+/**
+ * Scrapes the User Instagram posts using its ID.
+ *
+ * @param {string} user_id - The user ID on IG.
+ * @param {number} page_size - The number of Posts to retrieve.
+ *
+ * @returns {Promise<void>} - A promise that resolves when the scraping is complete.
+ */
+async function scrape_user_posts(user_id, page_size = 12) {
   const base_url =
     "https://www.instagram.com/graphql/query/?query_hash=e769aa130647d2354c40ea6a439bfc08&variables=";
   let variables = {
@@ -273,34 +104,38 @@ async function scrape_user_posts(user_id: string, page_size = 12) {
     const url = `${base_url}${encodeURIComponent(JSON.stringify(variables))}`;
     try {
       const response = await axios.get(url);
-      const data = response.data;
-      const posts = data.data.user.edge_owner_to_timeline_media;
+      if (response.ok) {
+        const data = response.data;
+        const posts = data.data.user.edge_owner_to_timeline_media;
 
-      for (const post of posts.edges) {
-        const parsedPost = parse_instagram_post(post.node);
-        if (post.node.is_video === false) {
-          parsedPosts.push(parsedPost);
+        for (const post of posts.edges) {
+          const parsedPost = parse_instagram_post(post.node);
+          if (post.node.is_video === false) {
+            parsedPosts.push(parsedPost);
+          }
         }
-      }
 
-      const page_info = posts.page_info;
+        const page_info = posts.page_info;
 
-      if (_page_number === 1) {
-        console.log(`scraping total ${posts.count} posts of ${user_id}`);
+        if (_page_number === 1) {
+          console.log(`scraping total ${posts.count} posts of ${user_id}`);
+        } else {
+          console.log(`scraping page ${_page_number}`);
+        }
+
+        if (!page_info.has_next_page || parsedPosts.length >= 21) {
+          break;
+        }
+
+        if (variables.after === page_info.end_cursor) {
+          break;
+        }
+
+        variables.after = page_info.end_cursor;
+        _page_number++;
       } else {
-        console.log(`scraping page ${_page_number}`);
+        throw Error("The fetch failed omegalul");
       }
-
-      if (!page_info.has_next_page) {
-        break;
-      }
-
-      if (variables.after === page_info.end_cursor) {
-        break;
-      }
-
-      variables.after = page_info.end_cursor;
-      _page_number++;
     } catch (error) {
       console.error("Error while scraping:", error);
       break;
@@ -308,10 +143,17 @@ async function scrape_user_posts(user_id: string, page_size = 12) {
   }
 }
 
-async function downloadImage(
-  url: string,
-  dest: string,
-): Promise<Root | undefined> {
+/**
+ * Downloads an image from the specified URL and saves it to the destination path.
+ *
+ * @param {string} url - The URL of the image to download.
+ * @param {string} dest - The destination path where the image will be saved.
+ *
+ * @returns {Promise<void>} - A promise that resolves when the image is successfully downloaded and saved.
+ * @throws {Error} - If there is an error during the download or saving process.
+ *
+ */
+async function downloadImage(url, dest) {
   const writer = fs.createWriteStream(dest);
 
   const response = await axios({
@@ -328,7 +170,16 @@ async function downloadImage(
   });
 }
 
-function parse_instagram_post(data: Post) {
+/**
+ * Parses an Instagram post and performs various actions, including downloading an image.
+ *
+ * @param {Post} data - The Instagram post data to be parsed.
+ * @returns {void} - The function doesn't return a value directly; it performs asynchronous actions.
+ *
+ * @throws {Error} - If there is an error during the download or file operations.
+ *
+ */
+function parse_instagram_post(data) {
   const imageDir = "../public/images";
   if (!fs.existsSync(imageDir)) {
     fs.mkdirSync(imageDir);
@@ -344,7 +195,7 @@ function parse_instagram_post(data: Post) {
     );
 
     let title = "";
-    const tags: string[] = [];
+    const tags = [];
 
     if (captions.length > 0) {
       const firstCaption = captions[0];
@@ -385,19 +236,19 @@ function parse_instagram_post(data: Post) {
         const types = `
 export type Posts = Post[];
 export interface Post {
-id: string;
-title: string,
-tags: string;
-created_at: number;
-shortcode: string;
-dimensions: Dimensions;
-img_src: string;
-ignore?: boolean;
+    id: string;
+    title: string,
+    tags: string;
+    created_at: number;
+    shortcode: string;
+    dimensions: Dimensions;
+    img_src: string;
+    ignore?: boolean;
 }
 
 export interface Dimensions {
-height: number;
-width: number;
+    height: number;
+    width: number;
 }
 `;
 
@@ -422,13 +273,13 @@ width: number;
             }
 
             // Parse the existing data
+            /** @type {Post[]} */
             const existingPosts = JSON.parse(
               dataFromFile.slice(dataFromFile.indexOf("[")),
             );
             // Check if a post with the same shortcode exists
             const existingPostIndex = existingPosts.findIndex(
-              (post: typeof parsedData) =>
-                post.shortcode === parsedData.shortcode,
+              (post) => post.shortcode === parsedData.shortcode,
             );
             if (existingPostIndex === -1) {
               // Append the new post data
@@ -465,13 +316,35 @@ width: number;
   }
 }
 
-function parseUser(data: any) {
+/**
+ * Parses Instagram user data.
+ *
+ * @param {Object} data - Instagram user data.
+ * @property {string} data.full_name - The full name of the user.
+ * @property {string} data.username - The username of the user.
+ * @property {string} data.id - The ID of the user.
+ * @property {string} data.biography - The biography of the user.
+ * @property {Array<Object>} data.bio_links - An array of objects representing bio links, each having a 'url' property.
+ * @property {string} data.external_url - The external URL of the user's homepage.
+ * @property {string} data.profile_pic_url_hd - The URL of the user's high-definition profile image.
+ *
+ * @returns {Object} - The parsed user data.
+ * @property {string} name - The full name of the user.
+ * @property {string} username - The username of the user.
+ * @property {string} id - The ID of the user.
+ * @property {string} bio - The biography of the user.
+ * @property {Array<string>} bio_links - An array of bio links.
+ * @property {string} homepage - The external URL of the user's homepage.
+ * @property {string} profile_image - The URL of the user's high-definition profile image.
+ *
+ */
+function parseUser(data) {
   const parsedData = {
     name: data.full_name,
     username: data.username,
     id: data.id,
     bio: data.biography,
-    bio_links: (data.bio_links || []).map((link: any) => link.url),
+    bio_links: (data.bio_links || []).map((link) => link.url),
     homepage: data.external_url,
     profile_image: data.profile_pic_url_hd,
   };
